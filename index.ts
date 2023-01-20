1c8e08ca1cc843cd29d28666c26c1ba7c6fe5f00
Mackenzie Gray <mdkulen@gmail.com>
2023-01-20 16:34:13 +1100

    added commented notes to index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 // A type is an easy way to refer to the different properties and functions that a value has
-
 // A value is anything we can assign to a variable (function, array, class etc)
-
 // Every value in typescript has a type  
 
 // Two kinds of types:
@@ -20,3 +18,4 @@
     // Classes
     // Objects
 
+// Types are used by the TypeScript compiler to analyse our code for errors and allows other developers to understand what values are defined in our codebase
