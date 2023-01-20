9520d45b3908c2678d232b12363c7f932889bace
Mackenzie Gray <mdkulen@gmail.com>
2023-01-20 16:29:30 +1100

    added index.ts file

---
+++ index.ts
@@ -0,0 +1,22 @@
+// A type is an easy way to refer to the different properties and functions that a value has
+
+// A value is anything we can assign to a variable (function, array, class etc)
+
+// Every value in typescript has a type  
+
+// Two kinds of types:
+  // Primitive types:
+    // Number 
+    // Boolean
+    // Void
+    // Undefined
+    // String 
+    // Symbol
+    // Null
+  
+  // Object types:
+    // Functions 
+    // Arrays
+    // Classes
+    // Objects
+
